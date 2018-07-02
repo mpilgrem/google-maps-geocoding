961ca55b10bd95ddb3564da774d78d5593100194
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2018-07-02 23:54:56 +0100

    Fix documentation, broken by CPP

--- src/Web/Google/Maps/Geocoding.hs
+++ src/Web/Google/Maps/Geocoding.hs
@@ -38,52 +38,51 @@
 -- > import Network.HTTP.Client (Manager, newManager)
 -- > import Network.HTTP.Client.TLS (tlsManagerSettings)
 -- > import Web.Google.Maps.Geocoding (Address (..), geocode, GeocodingResponse (..),
--- >     Geometry (..), Key (..), LatLng (..), Result (..), Status (..))
+-- >   Geometry (..), Key (..), LatLng (..), Result (..), Status (..))
 -- > import Web.Google.Static.Maps (Center (..), Location (..), Size (..),
--- >     staticmap, Zoom (..))
+-- >   staticmap, Zoom (..))
 -- > import System.IO (hFlush, stdout)
 -- >
 -- > main :: IO ()
 -- > main = do
--- >     putStrLn "A test of the Google Maps Geocoding API.\nNB: The use of the \
--- >         \API services is subject to the Google Maps APIs Terms of Serivce at \
--- >         \https://developers.google.com/maps/terms.\n"
--- >     txt <- input "Enter full address: "
--- >     mgr <- newManager tlsManagerSettings
--- >     let apiKey = Key "<REPLACE_THIS_WITH_YOUR_ACTUAL_GOOGLE_API_KEY>"
--- >     result <- geocode mgr apiKey (Just $ Address txt) Nothing Nothing
--- >         Nothing Nothing
--- >     case result of
--- >         Right response -> do
--- >             let s = status response
--- >             case s of
--- >                 OK -> do
--- >                     let latlng = location $ geometry $ head $ results
--- >                             response
--- >                         center = Center $ Coord latlng
--- >                     print center
--- >                     displayMap mgr apiKey center
--- >                 _  -> putStrLn $ "Error! Status: " ++ show s
--- >         _ -> putStrLn $ "Error! Result:\n" ++ show result
+-- >   putStrLn $ "A test of the Google Maps Geocoding API.\nNB: The use of " ++
+-- >     "the API services is subject to the Google Maps APIs Terms of " ++
+-- >     "Serivce at https://developers.google.com/maps/terms.\n"
+-- >   txt <- input "Enter full address: "
+-- >   mgr <- newManager tlsManagerSettings
+-- >   let apiKey = Key "<REPLACE_THIS_WITH_YOUR_ACTUAL_GOOGLE_API_KEY>"
+-- >   result <- geocode mgr apiKey (Just $ Address txt) Nothing Nothing
+-- >     Nothing Nothing
+-- >   case result of
+-- >     Right response -> do
+-- >       let s = status response
+-- >       case s of
+-- >         OK -> do
+-- >           let latlng = location $ geometry $ head $ results response
+-- >               center = Center $ Coord latlng
+-- >           print center
+-- >           displayMap mgr apiKey center
+-- >         _  -> putStrLn $ "Error! Status: " ++ show s
+-- >     _ -> putStrLn $ "Error! Result:\n" ++ show result
 -- >
 -- > input :: Text -> IO Text
 -- > input msg = T.putStr msg >> hFlush stdout >> T.getLine
 -- >
 -- > displayMap :: Manager -> Key -> Center -> IO ()
 -- > displayMap mgr apiKey center = do
--- >     let zoom = Just $ Zoom 17
--- >         w    = 400
--- >         h    = 400
--- >         size = Size w h
--- >     result <- staticmap mgr apiKey Nothing (Just center) zoom size Nothing
--- >         Nothing [] Nothing [] [] Nothing
--- >     case result of
--- >         Right response -> do
--- >             let picture = fromJust $ fromDynamicImage response
--- >                 title   = "Test Google Maps Geocoding API"
--- >                 window  = InWindow title (w, h) (10, 10)
--- >             display window white picture
--- >         Left err -> putStrLn $ "Error while displaying map: " ++ show err
+-- >   let zoom = Just $ Zoom 17
+-- >       w    = 400
+-- >       h    = 400
+-- >       size = Size w h
+-- >   result <- staticmap mgr apiKey Nothing (Just center) zoom size Nothing
+-- >     Nothing [] Nothing [] [] Nothing
+-- >   case result of
+-- >     Right response -> do
+-- >       let picture = fromJust $ fromDynamicImage response
+-- >           title   = "Test Google Maps Geocoding API"
+-- >           window  = InWindow title (w, h) (10, 10)
+-- >       display window white picture
+-- >     Left err -> putStrLn $ "Error while displaying map: " ++ show err
 module Web.Google.Maps.Geocoding
     ( -- * Functions
       geocode
